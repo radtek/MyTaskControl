14531cf9b62d61572808dc98880968543deb5c45
AF <1197827679@qq.com>
2019-04-04 16:03:43 +0800

    增加SQLite访问基类

---
+++ TaskUtility/DbAccess/SQLIteDbParameter.cs
@@ -0,0 +1,33 @@
+﻿/*===================================================
+* 类名称: SQLIteDbParameter
+* 类描述: SQLite参数对象类
+* 创建人: 李先锋
+* 创建时间: 2019/4/4 14:04:59
+* 修改人: 
+* 修改时间:
+* 版本： @version 1.0
+=====================================================*/
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskUtility.DbAccess
+{
+    /// <summary>
+    /// SQLite参数
+    /// </summary>
+    public class SQLIteDbParameter
+    {
+        /// <summary>
+        /// SQL语句
+        /// </summary>
+        public string SqlCmdTxt { get; set; }
+        /// <summary>
+        /// SQLite参数
+        /// </summary>
+        public List<SQLiteParameter> SqlLiteParameters { get; set; }
+    }
+}
---
+++ TaskUtility/DbAccess/SQLiteDbHandle.cs
@@ -0,0 +1,73 @@
+﻿/*===================================================
+* 类名称: SQLiteDbHandle
+* 类描述: SQLite数据操作基类
+* 创建人: 李先锋
+* 创建时间: 2019/4/4 14:04:38
+* 修改人: 
+* 修改时间:
+* 版本： @version 1.0
+=====================================================*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Data;
+
+namespace TaskUtility.DbAccess
+{
+    public class SQLiteDbHandle
+    {
+        /// <summary>
+        /// 增、删、改（SQLite）
+        /// </summary>
+        /// <param name="dbPath"></param>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public int SQLiteExecute(string dbPath,SQLIteDbParameter param)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection($"data source={dbPath}"))
+            {
+                conn.Open();
+                using (SQLiteCommand cmd = new SQLiteCommand(conn))
+                {
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.CommandText = param.SqlCmdTxt;
+                    foreach (var item in param.SqlLiteParameters)
+                    {
+                        cmd.Parameters.Add(item);
+                    }
+                    return cmd.ExecuteNonQuery();
+                }
+
+            }
+        }
+
+        public DataSet SQLiteGetDataSet(string dbPath,SQLIteDbParameter param)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection($"data source={dbPath}"))
+            {
+                conn.Open();
+                DataSet dt = new DataSet();
+                using (SQLiteDataAdapter da = new SQLiteDataAdapter())
+                {
+                    using (SQLiteCommand cmd = new SQLiteCommand())
+                    {
+                        cmd.CommandType = System.Data.CommandType.Text;
+                        cmd.CommandText = param.SqlCmdTxt;
+                        foreach (var item in param.SqlLiteParameters)
+                        {
+                            cmd.Parameters.Add(item);
+                        }
+                        da.SelectCommand = cmd;
+                    }
+                    da.Fill(dt);
+                    return dt;
+                }
+
+            }
+
+        }
+    }
+}
